256167a84b6fb597c7908b52e1aa76e2fa898a35
Nicolas Pageau <nic.pageau@hotmail.com>
2018-10-17 10:54:12 -0400

    Fini!!!

--- src/Main.java
+++ src/Main.java
@@ -23,26 +23,39 @@ public class Main extends Application {
         Label texte = new Label("Bienvenu  dans le modificateur d'image.");
 
         Menu fichier = new Menu("Fichiers");
+        Menu fichier1 = new Menu("Fichiers");
         Menu action = new Menu("Actions");
+        Menu action1 = new Menu("Actions");
         Menu charger = new Menu("Charger une image");
+        Menu charger1 = new Menu("Charger une image");
 
         Image image = new Image("default.jpg");
         Image image1 = new Image("image1.jpg");
+        Image image11 = new Image("image1.jpg");
         Image image2 = new Image("image2.jpg");
+        Image image22 = new Image("image2.jpg");
         Image image3 = new Image("image3.jpg");
+        Image image33 = new Image("image3.jpg");
 
         ImageView imageview =new ImageView(image);
         imageview.setFitWidth(500);
         imageview.setPreserveRatio(true);
 
         MenuItem reinitialise = new MenuItem("Réinitialiser");
+        MenuItem reinitialise1 = new MenuItem("Réinitialiser");
         MenuItem photo1 = new MenuItem("Image #1");
+        MenuItem photo11 = new MenuItem("Image #1");
         MenuItem photo2 = new MenuItem("Image #2");
+        MenuItem photo22 = new MenuItem("Image #2");
         MenuItem photo3 = new MenuItem("Image #3");
+        MenuItem photo33 = new MenuItem("Image #3");
 
         charger.getItems().addAll(photo1,photo2,photo3);
+        charger1.getItems().addAll(photo11,photo22,photo33);
         fichier.getItems().add(charger);
+        fichier1.getItems().add(charger1);
         action.getItems().add(reinitialise);
+        action1.getItems().add(reinitialise1);
 
         MenuBar mb = new MenuBar(fichier,action);
 
@@ -87,6 +100,21 @@ public class Main extends Application {
             imageview.setImage(image3);
             texte.setText("image 3 chargée");
         });
+        photo11.setOnAction((event)->{
+            chiffre.set(1);
+            imageview.setImage(image1);
+            texte.setText("image 1 chargée");
+        });
+        photo22.setOnAction((event)->{
+            chiffre.set(2);
+            imageview.setImage(image2);
+            texte.setText("image 2 chargée");
+        });
+        photo33.setOnAction((event)->{
+            chiffre.set(3);
+            imageview.setImage(image3);
+            texte.setText("image 3 chargée");
+        });
         ColorAdjust coloradjust = new ColorAdjust();
          luminosite.valueProperty().addListener(ov->{
              coloradjust.setBrightness(luminosite.getValue());
@@ -111,25 +139,24 @@ public class Main extends Application {
              satu.setValue(0);
              texte.setText("Réinitialisation des ajustement de couleur");
          });
-         ContextMenu ct = new ContextMenu(fichier,action);
-        imageview.setOnContextMenuRequested(event -> ct.show(imageview, event.getScreenX(), event.getScreenY()));
-
-
+        reinitialise1.setOnAction((event)->{
+            luminosite.setValue(0);
+            con.setValue(0);
+            teint.setValue(0);
+            satu.setValue(0);
+            texte.setText("Réinitialisation des ajustement de couleur");
+        });
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root);
+         ContextMenu ct = new ContextMenu(fichier1,action1);
+        scene.setOnContextMenuRequested(event -> ct.show(stage, event.getScreenX(), event.getScreenY()));
 
 
         HBox hb = new HBox(imageview,vb);
         hb.setAlignment(Pos.CENTER);
         hb.setSpacing(10);
-
-
         stage.setMaximized(true);
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root);
-        /*scene.setOnContextMenuRequested((ContextMenuEvent event) -> {
-            ct.show(stage), event.getScreenX(), event.getScreenY())
-        });
-        Il ne marche pas car le context menu dans la Menu bar est repris à la dernière place ou le clique droit a été fait.
-        */
+
         root.setTop(mb);
         root.setCenter(hb);
         root.setBottom(texte);
